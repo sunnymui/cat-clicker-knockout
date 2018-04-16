c4d7066dc8a57e026aed4da5b0d43d5201600440
Sunny Mui <sunnycmui@gmail.com>
2018-04-16 15:29:13 -0700

    define view model, apply bindings, define viewmodel observables, hook up observables with data bindings in the dom index.html, also rename cat picture names to more descriptive/sane file names, computed observable to display current cat level with corresponding dom element added/binding

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// initiate a new view model function
+var ViewModel = function() {
+  // track number of clicks on cat
+  this.clickCount = ko.observable(0);
+  // track current cat name
+  this.name = ko.observable('Abby');
+  // current cat image
+  this.img = ko.observable('img/blackwhite.jpg');
+  // current cat author / img source
+  this.img_author = ko.observable('https://www.flicker.com');
+  // strings to use for cat level display
+  this.cat_levels_list = {
+    0:'Newborn',
+    10:'infant',
+    20:'teen',
+    30:'adult',
+    40:'senior'
+  };
+  // for storing the current cat level text
+  this.current_cat_level = ko.observable('');
+
+  this.clicked = function() {
+    // increments the clickCount on clicks of bound cat dom element
+
+    // set the click count to current click count + 1
+    this.clickCount(this.clickCount() + 1);
+  };
+
+
+  this.cat_level = ko.computed(function(){
+    // compute the current cat level to use based on clicks
+
+    // check if the current click count is equal to a click count in the cat levels list
+    // only need to change cat level text when clicks reach defined points in our list
+    if (this.cat_levels_list[this.clickCount()]) {
+      // set the current cat level text to the corresponding string in the cat_levels_list
+      this.current_cat_level(this.cat_levels_list[this.clickCount()]);
+    }
+  }, this);
+};
+
+// bind our dom elements to our view model
+ko.applyBindings(new ViewModel());
